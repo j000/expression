e12d3c4ed67605f76660af008358f5f8d4ff8df4
j000 <jarymut+github@gmail.com>
2017-12-29 13:17:56 +0100

    Test stack only in debug mode

--- main.c
+++ main.c
@@ -7,28 +7,34 @@
 #include "stack.h"
 
 int main(int argc, char **argv) {
-	stack_t stack = stack_new(sizeof(double));
-	double a = 0;
+#ifdef _DEBUG
+	{
+		stack_t stack = stack_new(sizeof(double));
+		double a = 0;
 
-	stack_push(stack, &a);
-	a = 1;
-	stack_push(stack, &a);
-	stack_push(stack, &(double){2. });
-	stack_push(stack, &(double){3 });
-	double t = 0;
-	assert(!stack_is_empty(stack));
-	stack_pop(stack, &t);
-	assert(t == 3.);
-	assert(!stack_is_empty(stack));
-	stack_pop(stack, &t);
-	assert(t == 2.);
-	assert(!stack_is_empty(stack));
-	stack_pop(stack, &t);
-	assert(t == 1.);
-	assert(!stack_is_empty(stack));
-	stack_pop(stack, &t);
-	assert(t == 0.);
-	assert(stack_is_empty(stack));
+		stack_push(stack, &a);
+		a = 1;
+		stack_push(stack, &a);
+		stack_push(stack, &(double){2. });
+		stack_push(stack, &(double){3 });
+
+		double t = 0;
+
+		assert(!stack_is_empty(stack));
+		stack_pop(stack, &t);
+		assert(t == 3.);
+		assert(!stack_is_empty(stack));
+		stack_pop(stack, &t);
+		assert(t == 2.);
+		assert(!stack_is_empty(stack));
+		stack_pop(stack, &t);
+		assert(t == 1.);
+		assert(!stack_is_empty(stack));
+		stack_pop(stack, &t);
+		assert(t == 0.);
+		assert(stack_is_empty(stack));
+	}
+#endif /* ifdef _DEBUG */
 
 	return EXIT_SUCCESS;
 }
